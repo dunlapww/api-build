850c5f9d937bba818089c8fdf5f2bd8dbe26603c
Will Dunlap <dunlapww@gmail.com>
2020-12-11 11:48:56 -0700

    Pass items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    #render json: ItemSerializer.new(Item.all).serializable_hash
     render json: ItemSerializer.new(Item.all).serialized_json
   end
 end
