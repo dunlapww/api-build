575b98ed4211d7f13e1c55b2d4237f7961b03a68
Will Dunlap <dunlapww@gmail.com>
2020-12-10 18:02:10 -0700

    create migrations and db:seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,29 @@ puts "Loading PostgreSQL Data dump into local database with command:"
 puts cmd
 system(cmd)
 
+csv_text = File.read(Rails.root.join("db", "data", "items.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+csv.each do |item|
+  i = Item.new
+  i.id = item["id"]
+  i.name = item["name"]
+  i.description = item["description"]
+  i.unit_price = item["unit_price"]
+  i.merchant_id = item["merchant_id"]
+  i.created_at = item["created_at"]
+  i.updated_at = item["updated_at"]
+  i.save
+end
+
+#this sets all the id for all tables to be the max values in the table. So that when you generate a new record, it will do so after the last record id. (rather than start 0)
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+
+
+
+
 # TODO
 # - Import the CSV data into the Items table
 # - Add code to reset the primary key sequences on all 6 tables (merchants, items, customers, invoices, invoice_items, transactions)
