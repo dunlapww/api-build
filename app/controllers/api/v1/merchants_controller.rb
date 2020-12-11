717138ba81c6481e4bfa643e6c3a51b66da36bf1
Will Dunlap <dunlapww@gmail.com>
2020-12-11 14:44:09 -0700

    create and pass test for merchant show

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all).serialized_json
   end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id])).serialized_json
+  end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -33,4 +33,29 @@ describe 'Merchants API' do
     merchants = JSON.parse(response.body, symbolize_names: true)
     expect(merchants).to eq({:data=>[]})
   end
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+
+    expect(merchant).to be_a(Hash)
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data]).to have_key(:type)
+    expect(merchant[:data][:type]).to eq(Merchant.name.downcase)
+    expect(merchant[:data]).to have_key(:attributes)
+    
+    expect(merchant[:data][:attributes]).to be_a(Hash)
+    expect(merchant[:data][:attributes].count).to eq(3)
+
+    merchant_dtl = merchant[:data][:attributes]
+   
+    expect(merchant_dtl).to have_key(:name)
+    expect(merchant_dtl[:name]).to be_a(String)
+    expect(merchant_dtl[:created_at]).to be_a(String)
+    expect(merchant_dtl).to have_key(:updated_at)
+    expect(merchant_dtl[:updated_at]).to be_a(String)
+  end
 end
