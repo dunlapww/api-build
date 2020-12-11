ee8d91ff7e14ddb3ff934986560c27d50befb65f
Will Dunlap <dunlapww@gmail.com>
2020-12-11 14:00:34 -0700

    add merchants test and pass merchants test

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'sends a list of all merchants' do
+    create_list(:item, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(merchants[:data].count).to eq(3)
+    
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(String)
+      expect(merchant[:type]).to be_a(String)
+      expect(merchant[:type]).to eq(Merchant.name.downcase)
+      merchant[:attributes] do |attribute|
+        expect(attribute).to have_key(:name)
+        expect(attribute[:name]).to be_a(String)
+        expect(attribute[:created_at]).to be_a(String)
+        expect(attribute).to have_key(:updated_at)
+        expect(attribute[:updated_at]).to be_a(String)
+      end
+    end
+  end
+  it 'when there are no records in table it returns {:data=>[]}' do
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants).to eq({:data=>[]})
+  end
+end
