5801e5581b77dff00beb24a4a67160178b058148
Will Dunlap <dunlapww@gmail.com>
2020-12-11 10:37:42 -0700

    Add item and merchant factories

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Games::Zelda.item  }
+    description { Faker::Games::StreetFighter.unique.quote  }
+    unit_price { (rand() * 1000).round(2)}
+    merchant
+    created_at { Time.zone.today }
+    updated_at { Time.zone.today }
+  end
+end
