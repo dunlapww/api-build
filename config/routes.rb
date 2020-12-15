4e22de807b47137e5bf01ea9088cef37d08b458c
Will Dunlap <dunlapww@gmail.com>
2020-12-14 17:51:47 -0700

    schema and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index]
-      resources :merchants, only: [:index, :show, :create, :destroy]
+      resources :merchants, only: [:index, :show, :create, :destroy, :update]
     end
   end
 end
