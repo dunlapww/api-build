4e4c88f3a44d17327ebdbc3ab5efc0e085fe7dec
Will Dunlap <dunlapww@gmail.com>
2020-12-14 18:13:57 -0700

    manual patch route for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index]
-      resources :merchants, only: [:index, :show, :create, :destroy, :update]
+      resources :merchants, only: [:index, :show, :create, :destroy]
+      patch "/merchants/:id", to: 'merchants#update'
     end
   end
 end
