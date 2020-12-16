9ae69af13d4b57d1c45b1f92e0d12a972e711ec6
Will Dunlap <dunlapww@gmail.com>
2020-12-15 18:40:51 -0700

    Pass merchant search model test

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
 
   validates :name, presence: true
+
+  def self.search(search_term)
+    Merchant.where("UPPER(name) LIKE (?)","%#{search_term.upcase}%" )
+  end
 end
