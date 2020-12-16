62b9b3a57a0c273bd4ee438ca262451527bd35ee
Will Dunlap <dunlapww@gmail.com>
2020-12-16 15:59:20 -0700

    Refactor routes for item merchants and merchant items

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,17 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      scope :revenue do
+        get "/", to: 'revenue#revenue'
+      end
       namespace :merchants do
         get '/find_all', to: 'search#show'
       end
       resources :merchants do
-        get "/items", to: 'merchant_items#index'
+        get "/items", to: 'merchants/items#index'
       end
       resources :items, only: [:index] do
-        get '/merchants', to: 'items_merchant#index'
+        get '/merchants', to: 'items/merchants#index'
       end
     end
   end
