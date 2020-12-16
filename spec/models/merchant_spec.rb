0be257f1dff39852c6318f3ca149ba669adb7802
Will Dunlap <dunlapww@gmail.com>
2020-12-15 18:40:36 -0700

    Add merchant search model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,15 @@ describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'class methods' do
+    it '.search' do
+      merchant1 = create(:merchant, name: "Great Merchant")
+      merchant2 = create(:merchant, name: "Neat Merchant")
+      merchant3 = create(:merchant, name: "Harold's")
+      expect(Merchant.search('erch')).to eq([merchant1, merchant2])
+      expect(Merchant.search('har')).to eq([merchant3])
+      expect(Merchant.search('jsklsj')).to eq([])
+    end
+  end
 end
