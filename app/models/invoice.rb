a6000e83b0527795ac14f3ed4e14d25958fb9d21
Will Dunlap <dunlapww@gmail.com>
2020-12-16 15:57:00 -0700

    Pass test for invoice revenue method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,14 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
   has_many :transactions, dependent: :destroy
+  
+
+  def self.revenue(start_date, end_date)
+    Invoice.joins(:invoice_items, :transactions)
+            .where("result = ?","success")
+            .where("status = ?","shipped")
+            .where(created_at: (Date.parse(start_date).beginning_of_day..Date.parse(end_date).end_of_day))
+            .sum("invoice_items.quantity  * invoice_items.unit_price")    
+  end
+
 end
