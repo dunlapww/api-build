64776c91ad8bbe6f322e5300300e877d5253f070
Will Dunlap <dunlapww@gmail.com>
2020-12-16 20:45:27 -0700

    Add model test for merchant revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -73,12 +73,41 @@ describe Merchant, type: :model do
     it '.most_revenue' do 
       expect(Merchant.most_revenue(3)).to eq([@m5, @m4, @m3])
     end
-
+    
+    
     it '.most_items' do
       ii7 = create(:invoice_item, invoice: @iv2, item: @it2, quantity: 100, unit_price: 2) #rev = $40
       ii8 = create(:invoice_item, invoice: @iv3, item: @it3, quantity: 2000, unit_price: 3) #rev = $90
       ii9 = create(:invoice_item, invoice: @iv4, item: @it4, quantity: 140, unit_price: 4) #rev = $160
       expect(Merchant.most_items(3)).to eq([@m3, @m4, @m2])
+    end
+  end
+    
+  describe 'another class method' do
+    before :each do
+      #6 merchants
+      @m1 = create(:merchant)
+      
+      #an item created for each merchant
+      @it1 = create(:item, merchant: @m1)
+      
+
+      #an invoice for each merchant
+      @iv1 = create(:invoice, merchant: @m1, status: 'shipped', created_at: '2020-01-01T00:00:00 UTC')
+      @iv2 = create(:invoice, merchant: @m1, status: 'shipped', created_at: '2020-01-01T00:00:00 UTC')
+
+      #an invoice_item for each invoice, low quantity and price to high quantity and price
+      @ii1 = create(:invoice_item, invoice: @iv1, item: @it1, quantity: 10, unit_price: 1) #rev = $10
+      @ii2 = create(:invoice_item, invoice: @iv1, item: @it1, quantity: 20, unit_price: 2) #rev = $40
+      @ii3 = create(:invoice_item, invoice: @iv1, item: @it1, quantity: 30, unit_price: 3) #rev = $90
+      @ii4 = create(:invoice_item, invoice: @iv2, item: @it1, quantity: 30, unit_price: 3) #rev = $90
+
+      #a transaction for each invoice
+      @t1 = create(:transaction, invoice: @iv1, result: 'success')
+      @t2 = create(:transaction, invoice: @iv2, result: 'failure')
+    end
+    it '.revenue' do 
+      expect(Merchant.revenue(@m1.id).round(0)).to eq(140)
     end
   end
 end
